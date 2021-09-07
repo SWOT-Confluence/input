476a98fd2d6b580230e5ce7e93c07fd938ac085a
Nikki Tebaldi <ntebaldi@umass.edu>
2021-09-07 08:03:24 -0400

    Prints current status

--- run_input.py
+++ run_input.py
@@ -38,27 +38,33 @@ def main():
         pull = "no pull"
 
     # Login
+    print("Logging into AWS infrastructure.")
     login = Login()
     login.login()
 
     # Extract SWOT data
+    print("Extracting SWOT data.")
     ext = Extract()
     ext.extract_data(login.confluence_fs)
     
-    # Copy SoS data and Write SWOT data
+    # Write SWOT data and copy SoS data
+    print("Writing SWOT data to NetCDF.")
     write = Write(ext.node_data, ext.reach_data, OUTPUT)
-    write = Write(None, None, OUTPUT / "output")
-    write.copy_sos_data(login.confluence_fs, run_type)
     write.write_data()
-
+    print("Downloading SoS.")
+    write.copy_sos_data(login.confluence_fs, run_type)
+    
     # Append USGS gage data to the SoS
     if run_type == "constrained" and pull == "pull":
+        print("Pulling USGS gage data and appending to SoS.")
         gage_pull = GagePull(OUTPUT / "usgs" / "USGStargetsV3.nc", '1980-1-1', datetime.today().strftime("%Y-%m-%d"))
         gage_pull.pull()
         gage_append = GageAppend(OUTPUT / "sos", gage_pull.usgs_dict)
         gage_append.read_sos()
         gage_append.map_data()
         gage_append.append_data()
+
+    print("Input operations complete.")
 
 if __name__ == "__main__":
     
