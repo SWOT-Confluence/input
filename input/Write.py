f04111860153ea6c919bcc7d0250d41e789924b1
Nikki Tebaldi <ntebaldi@umass.edu>
2021-09-07 08:14:03 -0400

    Added run_type

--- input/Write.py
+++ input/Write.py
@@ -1,8 +1,5 @@
 # Standard imports
 from datetime import datetime
-import json
-from pathlib import Path
-from tempfile import TemporaryDirectory
 
 # Third-party imports
 from netCDF4 import Dataset
@@ -68,16 +65,18 @@ class Write:
         self.output_dir = output_dir
         self.reach_data = reach_data
 
-    def copy_sos_data(self, confluence_fs):
+    def copy_sos_data(self, confluence_fs, run_type):
         """Copy S3 SoS data to output directory.
         
         Parameters
         ----------
         confluence_fs: S3FileSystem
             references Confluence S3 buckets
+        run_type: str
+            either "constrained" or "unconstrained"
         """
         
-        dirs = confluence_fs.ls("confluence-sos")
+        dirs = confluence_fs.ls(f"confluence-sos/{run_type}")
         curr_dir = max(dirs)
         files = confluence_fs.glob(f"{curr_dir}/*.nc")
         for file in files:
