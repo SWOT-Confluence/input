f79d9474d51fbec79715d15d14e566171db7f21c
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2022-11-21 14:08:02 -0500

    Fix geopandas issue with reading from S3.
    - Implement local read
    - Read zipped shapefile dbf portion only
    - Use pandas for dataframe creation and manipulation

--- input/extract/ExtractStrategy.py
+++ input/extract/ExtractStrategy.py
@@ -10,144 +10,49 @@ class ExtractStrategy(metaclass=ABCMeta):
 
     Attributes
     ----------
-    confluence_fs: S3FileSystem
-        references Confluence S3 buckets
-    cycle_data: dict
-        dictionary of cycle identifier keys with pass identifier values
-    obs_times: list
-        list of 'cycle/pass' values that contained observations for this reach
+    swot_id: int
+            unique SWOT identifier (identifies continent)       
+    cycle_pass: list
+        list of cycle passes in the form "c_p".
+    shapefiles: list
+        list of SWOT shapefiles
 
     Methods
     -------
-    extract(confluence_fs)
-        extracts data from S3 bucket shapefiles and stores in data dictionaries
-    extract_passes(continent)
-        retrieve pass and cycle identifiers for continent from shapefiles.
+    extract()
+        extracts data from S3 bucket shapefiles and stores in data dictionaries.
+    extract_local()
+        extracts data from local file system and stores in data dictionaries.
     """
     
-    # LOCAL_INPUT = Path("/mnt/data/shapefiles/swot/river")    # local
-    CONT_LOOKUP = { 1: "AF", 2: "EU", 3: "AS", 4: "AS", 5: "OC", 6: "SA", 
-                    7: "NA", 8: "NA", 9: "NA"}
-    
-    def __init__(self, confluence_fs, swot_id, cycle_pass_json):
+    def __init__(self, swot_id, shapefiles, cycle_pass, creds=None):
         """
         Parameters
         ----------
-        confluence_fs: S3FileSystem
-            references Confluence S3 buckets
         swot_id: int
-            unique SWOT identifier (identifies continent)
-        cycle_pass_json: Path
-            path to cycle pass JSON file
+            unique SWOT identifier (identifies continent)       
+        shapefiles: list
+            list of SWOT shapefiles
+        cycle_pass: dict
+            dictionary of cycle pass data
+        creds: dict
+            dictionary of AWS S3 credentials
         """
         
-        self.confluence_fs = confluence_fs        
-        self.cycle_data = self.extract_passes(int(str(swot_id)[0]))
-        # self.cycle_data = self.extract_passes_local(int(str(swot_id)[0]))    # local
+        self.swot_id = swot_id
+        self.shapefiles = shapefiles
         self.obs_times = []
-        with open(cycle_pass_json, 'r') as jf:
-            self.pass_data = json.load(jf)
+        self.cycle_pass = cycle_pass
+        self.creds = creds
     
     @classmethod
     def __subclasshook__(cls, subclass):
-        return (hasattr(subclass, 'extract_data') and 
-                callable(subclass.extract_data) and 
-                hasattr(subclass, 'extract_data_local') and 
-                callable(subclass.extract_data_local) and
-                hasattr(subclass, 'retrieve_swot_files') and 
-                callable(subclass.retrieve_swot_files) and 
-                hasattr(subclass, 'retrieve_swot_files_local') and 
-                callable(subclass.retrieve_swot_files_local) or
+        return (hasattr(subclass, 'extract') and 
+                callable(subclass.extract) or
                 NotImplemented)
 
     @abstractmethod
     def extract(self):
         """Extracts data from confluence_fs S3 bucket and stores in data dict."""
 
-        raise NotImplementedError
-    
-    @abstractmethod
-    def extract_local(self):
-        """Extracts data from SWOT shapefiles and stores in data dictionaries."""
-        
-        raise NotImplementedError    
-    
-    def extract_passes(self, c_id):
-        """Retrieve pass and cycle identifiers for continent from shapefiles.
-        
-        Parameters
-        ----------
-        c_id: int
-            Continent integer identifier
-        
-        Returns
-        -------
-        dictionary of cycle keys and pass values
-        """
-        
-        # Locate cycle/pass identifiers for continent
-        c_files = self.retrieve_swot_files(c_id)
-        c_dict = {}
-        for c_file in c_files:
-            key = int(c_file.split('_')[5])
-            if key in c_dict.keys():
-                c_dict[key].append(int(c_file.split('_')[6]))
-            else:
-                c_dict[key] = [int(c_file.split('_')[6])]
-        
-        # Sort pass identifiers for each cycle
-        for value in c_dict.values(): value.sort()
-        
-        return c_dict
-        
-    def extract_passes_local(self, c_id):
-        """Retrieve pass and cycle identifiers for continent from shapefiles.
-        
-        Parameters
-        ----------
-        c_id: int
-            Continent integer identifier
-        
-        Returns
-        -------
-        dictionary of cycle keys and pass values
-        """
-        
-        # Locate cycle/pass identifiers for continent
-        c_files = self.retrieve_swot_files_local(c_id)
-        c_dict = {}
-        for c_file in c_files:
-            key = int(c_file.split('_')[5])
-            if key in c_dict.keys():
-                c_dict[key].append(int(c_file.split('_')[6]))
-            else:
-                c_dict[key] = [int(c_file.split('_')[6])]
-        
-        # Sort pass identifiers for each cycle
-        for value in c_dict.values(): value.sort()
-        
-        return c_dict
-    
-    @abstractmethod
-    def retrieve_swot_files(self, c_id):
-        """Retrieve SWOT Lake shapefiles.
-        
-        Parameters
-        ----------
-        c_id: int
-            Continent integer identifier
-        """
-        
-        raise NotImplementedError
-    
-    @abstractmethod
-    def retrieve_swot_files_local(self, c_id):
-        """Retrieve SWOT Lake shapefiles.
-        
-        Parameters
-        ----------
-        c_id: int
-            Continent integer identifier
-        """
-        
         raise NotImplementedError
---
+++ input/extract/exceptions.py
@@ -0,0 +1,4 @@
+class ReachNodeMismatch(Exception):
+    """Raised when the observation times for a reach and corresponding nodes
+    do not match."""
+    pass
